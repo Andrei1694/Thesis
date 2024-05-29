317ca94dab97e2f3e481326427d9a792cf6c3327
Stanciu Andrei <andrei4.stanciu@gmail.com>
2024-05-30 01:09:40 +0300

    added build scripts

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -2,12 +2,29 @@ import express from 'express'
 import api from './routes/api.mjs'
 import { errorHandler } from './utils/middlewares.mjs'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import compression from 'compression'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
+
 app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(compression());
 app.use(express.json())
 
+// Serve the frontend build files
+app.use(express.static(path.join(__dirname, '..', 'build')))
+
 app.use('/v1', api)
+
+// Catch-all route to serve the frontend's index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'..', 'build', 'index.html'));
+  });
+  
 app.use(errorHandler)
 
 export default app
