099ed8d23d527845b2d9a98ce4ce31991fcad2f9
Andrei1694 <andrei4.stanciu@gmail.com>
2024-06-01 01:19:48 +0300

    improved some responsivnes

--- frontend/src/components/button.component.jsx
+++ frontend/src/components/button.component.jsx
@@ -2,7 +2,7 @@ function Button({ color, onClick, children, className, type }) {
   return (
     <button
       type={type}
-      className={`bg-customPrimary w-[327px] h-12 rounded-[15px] hover:bg-customSecondary ${className}`}
+      className={`bg-customPrimary w-min-[327px] w-[100%] h-12 rounded-[15px] hover:bg-customSecondary ${className}`}
       onClick={onClick}
     >
       <div className="text-center text-white text-base font-bold leading-tight">
--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -8,9 +8,9 @@ import { getAuthToken, removeAuthToken } from "../utils/auth";
 
 function UserLogo({ firstName, lastName }) {
   return (
-    <div className="flex items-center max-h-100 w-[200px] h-[50px]">
-      <div className="ml-1">
-        <img className="rounded-full w-10 h-10" src="https://picsum.photos/200/300" alt="img" />
+    <div className="flex items-center justify-center">
+      <div className="mr-4 w-[40px] h-[40px]">
+        <img className="rounded-full w-100 h-100" src="https://picsum.photos/200/300" alt="User Avatar" />
       </div>
       <span className="text-white ml-auto mr-2 whitespace-nowrap">{firstName + " " + lastName}</span>
     </div>
@@ -80,184 +80,68 @@ const Navbar = () => {
     inputRef.current?.focus();
   };
 
+
+
+
+  //       <button
+  //         className="text-white hover:bg-customSecondary px-3 py-2 rounded-md text-sm font-medium"
+  //         onClick={logoutUser}
+  //       >
+  //         Logout
+  //       </button>
+  //     </>
+  //   )
+  // }
+  // {
+  //   !isAuthenticated && (
+  //     <Link to="/login" className="text-white hover:bg-customSecondary px-3 py-2 rounded-md text-sm font-medium">
+  //       Login
+  //     </Link>
+
   return (
-    <nav className="bg-customPrimary">
-      {/* ... */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-              <span className="text-white font-bold text-xl cursor-pointer" onClick={() => navigate("/devices")}>
-                Admin Panel
-              </span>
-            </div>
-            <div className="hidden md:block">
-              {isAuthenticated && (
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <Link
-                    to="/profile"
-                    className="text-white hover:bg-customSecondary px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    My Profile
-                  </Link>
-                  <Link
-                    to="/devices?page=1"
-                    className="text-white hover:bg-customSecondary px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Devices
-                  </Link>
-                  <Link
-                    to="/users"
-                    className="text-white hover:bg-customSecondary px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Users
-                  </Link>
-                  <button
-                    className="text-white hover:bg-customSecondary px-3 py-2 rounded-md text-sm font-medium"
-                    onClick={logoutUser}
-                  >
-                    Logout
-                  </button>
-                </div>
-              )}
-              {!isAuthenticated && (
-                <Link to="/login" className="text-white hover:bg-customSecondary px-3 py-2 rounded-md text-sm font-medium">
-                  Login
+    <nav className="bg-customPrimary w-min-[100vw]">
+      <div className="flex px-4 py-4 text-white">
+        <div className="flex items-center w-100">
+          {/* logo */}
+          <div className="text-lg whitespace-nowrap">
+            Admin Panel
+          </div>
+          <div className="ml-5 w-[100%]">
+            <ul className="flex">
+              <li className="mr-2">
+                <Link
+                  to="/profile"
+                  className="text-white hover:bg-customSecondary px-1 py-1 rounded-md text-sm font-medium"
+                >
+                  My Profile
                 </Link>
-              )}
-            </div>
+              </li>
+              <li className="mr-2">
+                <Link
+                  to="/devices?page=1"
+                  className="text-white hover:bg-customSecondary px-1 py-1 rounded-md text-sm font-medium"
+                >
+                  Devices
+                </Link>
+              </li>
+              <li className="mr-2">
+                <Link
+                  to="/users"
+                  className="text-white hover:bg-customSecondary px-1 py-1 rounded-md text-sm font-medium"
+                >
+                  Users
+                </Link>
+              </li>
+
+            </ul>
           </div>
-          <div className="hidden md:block">
-            <div className="ml-4 flex items-center md:ml-6">
-              <div className="relative">
-                <input
-                  type="text"
-                  placeholder="Search devices..."
-                  value={searchTerm}
-                  onChange={handleSearch}
-                  className="px-3 py-2 placeholder-gray-400 text-gray-900 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-customPrimary sm:text-sm"
-                />
-                {isLoading ? (
-                  <div className="absolute mt-1 w-full bg-white rounded-md shadow-lg z-10 p-2">Loading...</div>
-                ) : searchTerm && filteredDevices.length > 0 ? (
-                  <div className="absolute mt-1 w-full bg-white rounded-md shadow-lg z-10">
-                    {filteredDevices.map(({ _id, deviceName }) => (
-                      <div
-                        key={_id}
-                        onClick={() => {
-                          navigate(`/devices/${_id}`);
-                          clearSearch();
-                        }}
-                        className="px-4 py-2 text-sm text-gray-800 hover:bg-gray-100 cursor-pointer"
-                      >
-                        {deviceName}
-                      </div>
-                    ))}
-                  </div>
-                ) : null}
-              </div>
-            </div>
-          </div>
-          {isAuthenticated && (
+          <div className="ml-auto">
             <Link to="/profile">
               <UserLogo firstName={firstName} lastName={lastName} />
             </Link>
-          )}
-          <div className="-mr-2 flex md:hidden">
-            <Button
-              className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-customSecondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-customPrimary focus:ring-white"
-              onClick={toggleMobileMenu}
-            >
-              <span className="sr-only">Open main menu</span>
-              {isMobileMenuOpen ? (
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              ) : (
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-              )}
-            </Button>
           </div>
         </div>
       </div>
-
-      {isMobileMenuOpen && (
-        <div className="md:hidden">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <Link
-              to="/profile"
-              className="text-white hover:bg-customSecondary block px-3 py-2 rounded-md text-base font-medium"
-              onClick={toggleMobileMenu}
-            >
-              My Profile
-            </Link>
-            <Link
-              to="/devices"
-              className="text-white hover:bg-customSecondary block px-3 py-2 rounded-md text-base font-medium"
-              onClick={toggleMobileMenu}
-            >
-              Devices
-            </Link>
-            <Link
-              to="/users"
-              className="text-white hover:bg-customSecondary block px-3 py-2 rounded-md text-base font-medium"
-              onClick={toggleMobileMenu}
-            >
-              Users
-            </Link>
-            <button
-              className="text-white hover:bg-customSecondary block px-3 py-2 rounded-md text-base font-medium"
-              onClick={logoutUser}
-            >
-              Logout
-            </button>
-          </div>
-          <div className="relative">
-            <input
-              ref={inputRef}
-              type="text"
-              placeholder="Search devices..."
-              value={searchTerm}
-              onChange={handleSearch}
-              className="px-3 py-2 placeholder-gray-400 text-gray-900 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-customPrimary sm:text-sm"
-            />
-            {isLoading ? (
-              <div className="absolute mt-1 w-full bg-white rounded-md shadow-lg z-10 p-2">Loading...</div>
-            ) : searchTerm && filteredDevices.length > 0 ? (
-              <div className="absolute mt-1 w-full bg-white rounded-md shadow-lg z-10">
-                {filteredDevices.map(({ _id, deviceName }) => (
-                  <div
-                    key={_id}
-                    onClick={() => {
-                      navigate(`/devices/${_id}`);
-                      clearSearch();
-                    }}
-                    className="px-4 py-2 text-sm text-gray-800 hover:bg-gray-100 cursor-pointer"
-                  >
-                    {deviceName}
-                  </div>
-                ))}
-              </div>
-            ) : null}
-          </div>
-        </div>
-      )}
     </nav>
   );
 };
