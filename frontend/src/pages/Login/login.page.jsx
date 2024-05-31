28eea6c948c1dc61ce6aed7b74b650b1fd55b452
Andrei1694 <andrei4.stanciu@gmail.com>
2024-05-31 14:21:29 +0300

    test

--- frontend/src/pages/Login/login.page.jsx
+++ frontend/src/pages/Login/login.page.jsx
@@ -4,11 +4,12 @@ import { useMutation } from "react-query";
 import * as yup from "yup";
 import Button from "../../components/button.component";
 import AuthForm from "../../forms/auth.form";
-
+const tilte = import.meta.env.CEVA
 export default function LoginPage() {
   return (
     <div>
       <div className="container mt-5">
+        {tilte}
         <AuthForm />
       </div>
     </div>
