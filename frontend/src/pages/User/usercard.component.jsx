445b16f48d91db8715f4d44877bd1302984507c6
Andrei1694 <andrei4.stanciu@gmail.com>
2024-08-22 22:27:36 +0300

    fixed user card

--- frontend/src/pages/User/usercard.component.jsx
+++ frontend/src/pages/User/usercard.component.jsx
@@ -1,12 +1,31 @@
+import React from 'react';
+
+const getInitials = (firstName, lastName) => {
+  if (firstName && lastName) {
+    const firstInitial = firstName.charAt(0).toUpperCase();
+    const lastInitial = lastName.charAt(0).toUpperCase();
+    return `${firstInitial}${lastInitial}`;
+  }
+  return "";
+};
+
 export default function UserCard({ user }) {
+  const initials = getInitials(user.firstName, user.lastName);
+
   return (
     <div className="bg-white rounded-[10px] shadow p-6 mb-4">
       <div className="flex items-center mb-4">
-        <img
-          src={user.profileImage || "default-profile.png"}
-          alt={`${user.firstName} ${user.lastName}`}
-          className="w-20 h-20 rounded-full mr-4"
-        />
+        {user.profileImage ? (
+          <img
+            src={user.profileImage}
+            alt={`${user.firstName} ${user.lastName}`}
+            className="w-20 h-20 rounded-full mr-4 object-cover"
+          />
+        ) : (
+          <div className="w-20 h-20 rounded-full mr-4 bg-customLight flex items-center justify-center">
+            <span className="text-2xl font-bold text-white">{initials}</span>
+          </div>
+        )}
         <div>
           <h2 className="text-2xl font-bold text-customPrimary">
             {user.firstName} {user.lastName}
